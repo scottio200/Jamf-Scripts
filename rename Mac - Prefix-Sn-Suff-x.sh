10d4b484b85a8e7613a43c6af01f63386e55f8ac
Scott Tomlinson <37301523+scottio200@users.noreply.github.com>
2018-05-24 12:12:10 +0530

    Update rename Mac - Prefix-Sn-Suff-x.sh

--- rename Mac - Prefix-Sn-Suff-x.sh
+++ rename Mac - Prefix-Sn-Suff-x.sh
@@ -3,7 +3,7 @@
 # Created by Scott Tomlinson 15th November 2017
 #
 
-# Variables 
+# Variables - Use $4 and $5 for Prefix and Suffix. These are defined in Jamf Pro under the Policy>Script options
 prefix=$4
 serial="$(ioreg -l | grep IOPlatformSerialNumber | sed -e 's/.*\"\(.*\)\"/\1/')"
 suffix=$5
